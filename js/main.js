c45c0bb2dd878f0da38e5abf62cd60a1ba49d889
marykrach <80330561+marykrach@users.noreply.github.com>
2022-01-27 17:52:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  loop: true,
+  speed: 400,
+  spaceBetween: 70,
+  senteredSlides: true,
+  slideToClickedSlide: true,
+  toggle: true,
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //   }
+  // }
+
+});
